19483649a351a256e917af0077c0a47f439c33ff
Vildanov-Roman <vildanovv.rr@gmail.com>
2022-08-13 01:17:41 +0300

    homeWork7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// // Change code below this line
 
-const gallery = document.querySelector('.gallery')
-const items = []
+const gallery = document.querySelector(".gallery");
+const imagesMarkup = createItems(galleryItems);
+gallery.insertAdjacentHTML("beforeend", imagesMarkup);
 
-galleryItems.forEach(element => {
-	const galleryItem = document.createElement('div')
-	galleryItem.className = 'gallery__item'
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-    galleryImage.className = 'gallery__image'
-    galleryImage.src = element.preview;
-    galleryImage.setAttribute('data-source', element.original)
-    galleryImage.alt = element.description;
+function createItems(item) {
+	return galleryItems
+    .map(({ preview, original, description }) => {
+    	return `<div class="gallery__item">
+    	<a class="gallery__link" href="${original.value}">
+        <img
+        	class="gallery__image"
+        	src="${preview}"
+        	data-source="${original}"
+        	alt="${description}"
+        />
+    	</a>
+    </div>`;
+    })
+    .join("");
+}
 
-	galleryItem.append(galleryLink)
-	galleryLink.append(galleryImage)
-	items.push(galleryItem)
-})
 
-gallery.append(...items)
+gallery.addEventListener('click', evt => {
+    evt.preventDefault();
 
-gallery.addEventListener('click', e => {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
+    if (evt.target.nodeName !== 'IMG') {
 		return
 	}
-
-    const selectedImage = e.target.getAttribute('data-source')
-
+    const selectedImage = evt.target.getAttribute('data-source')
     const instance = basicLightbox.create(`
     <img src="${selectedImage}" width="800" height="600">
 `)
-
     instance.show()
     
-    gallery.addEventListener('keydown', e => {
-		if (e.key === 'Escape') {
+    gallery.addEventListener('keydown', evt => {
+		if (evt.key === 'Escape') {
 			instance.close()
 		}
 	})
 })
-
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,26 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector('.gallery')
-const items = []
+const gallery = document.querySelector(".gallery");
+const imagesMarkup = createItems(galleryItems);
+gallery.insertAdjacentHTML("beforeend", imagesMarkup);
 
-galleryItems.forEach(element => {
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-	galleryImage.className = 'gallery__image'
-	galleryImage.src = element.preview
-	galleryImage.setAttribute('title', element.description)
-	galleryImage.alt = element.description
+function createItems(item) {
+	return galleryItems
+    .map(({ preview, original, description }) => {
+    	return `<ul class="gallery">
+    	<li>
+			<a class="gallery__item" href="${original}">
+				<img
+					class="gallery__image"
+					src="${preview}"          
+					alt="${description}"
+				/>
+			</a>
+    	</li>    
+    </ul>`;
+    })
+    .join("");
+}
 
-	galleryLink.append(galleryImage)
-	items.push(galleryLink)
-})
-gallery.append(...items)
-
-new SimpleLightbox('.gallery a', {
-	captionDelay: 250
-})
-
-console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+	captionType: "alt",
+});
